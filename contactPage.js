7fabce0cb32dee1e02dd9e459d2ddf37a068a9a9
Eragraicevci <eg51908@ubt-uni.net>
2021-12-13 23:58:51 +0200

    contactPage

---
+++ contactPage.js
@@ -0,0 +1,88 @@
+
+    function isValidEmail(email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+      };
+
+      function validatePhoneNumber(telephone) {
+    var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    return re.test(telephone);
+}
+     
+      const form = document.querySelector("form[name='contact-form']");
+      const thankU=document.querySelector(".thankU-div");
+      const nameInput=document.querySelector('input[name="name"]');
+      const emailInput=document.querySelector('input[name="email"]');
+      const telInput=document.querySelector('input[name="telephone"]');
+      const messageInput=document.querySelector('textarea[name="comments"]');
+      
+     const inputs=[nameInput,emailInput,telInput,messageInput];
+     
+     /*document.querySelector('#email').value = "";
+     function emailat(){
+        for(let i = 0; i<emailInput.length; i++){
+            console.log(emailInput[i].email);
+        }
+    }*/
+  
+
+      let isFormValid=false;
+      let isValidationOn=false;
+
+      const resetElm=(elm)=>{
+           elm.classList.remove("invalid");
+        elm.nextElementSibling.classList.add("hidden");
+
+      };
+
+      const invalidateElm=(elm)=>{
+        elm.classList.add("invalid");
+     elm.nextElementSibling.classList.remove("hidden");
+    };
+      
+
+      const validoInputin =()=> {
+          if(!isValidationOn)return;
+        isFormValid=true;
+        //resetElm(nameInput);
+        //resetElm(emailInput);
+        //resetElm(telInput);
+        //me shkurt:
+        inputs.forEach(resetElm);
+
+
+        if(!nameInput.value){ 
+             isFormValid=false;
+            invalidateElm(nameInput);
+          }
+
+          if(!isValidEmail(emailInput.value)){ 
+             isFormValid=false;
+            invalidateElm(emailInput);
+          }
+
+          if(!validatePhoneNumber(telInput.value)){ 
+            isFormValid=false;
+           invalidateElm(telInput);
+         }
+
+         if(!messageInput.value){ 
+            isFormValid=false;
+           invalidateElm(messageInput);
+         }
+      };
+
+      form.addEventListener("submit", (e)=> {
+          e.preventDefault(); //qe mos me u refresh faqja
+          isValidationOn=true;
+          validoInputin();
+          if(isFormValid){
+              form.remove(); //e fshin komplet input formen dhe shfaqet thank u message
+              thankU.classList.remove("hidden");
+          }
+      });
+
+      //mos me qit te kuqe kur shkrujme emrin
+      inputs.forEach((input) => input.addEventListener("input", validoInputin));
+
+
