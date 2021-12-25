f634b2cb3f213b3ff607dc153db9b4236afb6fe4
Fitore <ttahirifitore@gmail.com>
2021-12-26 00:39:40 +0100

    updated

--- login.js
+++ login.js
@@ -1,9 +1,14 @@
 function logIn(){
+    var emailRegex=/[a-z]/;
+    var emailInput=document.getElementById('email').value;
+    var testEmail=emailRegex.test(emailInput);
+
     var passwordRegex=/^[A-Z]{1}[A-Za-z0-9]+\d{3}$/;
     var inputi =document.getElementById('password').value
     var testo=passwordRegex.test(inputi);
 
-    if(testo){
+
+    if(testo && testEmail){
         console.log('Regjistrimi u krye me sukses!')
         document.write('Jeni regjistruar me sukses!');
     }
