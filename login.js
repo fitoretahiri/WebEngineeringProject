a0f9d27e133a74b21b87f15c391e6a8086750d5b
Fitore <ttahirifitore@gmail.com>
2021-12-26 16:09:43 +0100

    updated

--- login.js
+++ login.js
@@ -1,23 +1,83 @@
-function logIn(){
-    var emailRegex=/[a-z]/;
-    var emailInput=document.getElementById('email').value;
-    var testEmail=emailRegex.test(emailInput);
-
-    var passwordRegex=/^[A-Z]{1}[A-Za-z0-9]+\d{3}$/;
-    var inputi =document.getElementById('password').value
-    var testo=passwordRegex.test(inputi);
-
-
-    if(testo && testEmail){
-        console.log('Regjistrimi u krye me sukses!')
-        document.write('Jeni regjistruar me sukses!');
+let emailInput=document.getElementById('email');
+let passwordInput=document.getElementById('password');
+let checkboxInput=document.getElementById('checkbox');
+let password2Input=document.getElementById('password2');
+let nameInput=document.getElementById('name');
+let surnameInput=document.getElementById('surname');
+let ageInput=document.getElementById('age');
+let birthdayInput=document.getElementById('birthday');
+//Validimi i faqes log in
+function validateLogIn(){
+    if(emailInput.value.trim()===''){
+        onError(emailInput,"Email cannot be empty!");
     }
     else{
-        console.log('Keni gabuar fjalekalimin apo emailin tuaj!');
-        document.write('Keni gabuar fjalekalimin apo emailin tuaj!');
+        if(!emailValidation(emailInput.value.trim())){
+            onError(emailInput,"Email is not valid");
+        }else{
+            onSuccess(emailInput);
+        }
+    }
+    if(passwordInput.value.trim()===''){
+        onError(passwordInput,"Password cannot be empty!")
+    }
+    else{
+        if(testWhiteSpaces(passwordInput.value)){
+            onError(passwordInput,"Password cannot contain white spaces!")
+        }
+      else if(!testLength(passwordInput.value)){
+            onError(passwordInput,"Password should be atleast 8 characters long!")
+        }
+        else{
+            onSuccess(passwordInput);
+        }
+    }
+    if(checkboxInput.checked ==false){
+        onError(checkboxInput,"Accept our terms of use!")
+    }
+    else{
+        onSuccess(checkboxInput);
     }
 }
-
-function register(){
-
+//Nese permbushen kerkesat
+function onSuccess(input){
+    let parent=input.parentElement;
+    let messageE=parent.querySelector("small");
+    messageE.style.visibility='hidden';
+    parent.classList.remove('error');
+    parent.classList.add('success');
 }
+
+//Nese nuk permbushen kerkesat
+function onError(input, message){
+    let parent=input.parentElement;
+    let messageE=parent.querySelector("small");
+    messageE.style.visibility='visible';
+    messageE.innerText=message;
+    parent.classList.add('error');
+    parent.classList.remove('success');
+}
+
+
+function emailValidation(emailInput){
+    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(emailInput);
+ }
+
+ function testWhiteSpaces(passwordInput){
+     return  /^(?=.*\s)/.test(passwordInput);
+ }
+function testLength(passwordInput){
+     return /^.{8,}$/.test(passwordInput);
+ }
+function testContentP(passwordInput){
+    return /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?~_+-=|\])..{8,32}$/.test(passwordInput);
+}
+//thirrja e funksionit ne buton
+
+
+document.querySelector("button")
+.addEventListener("click",(event)=>{
+    event.preventDefault();
+    validateLogIn();
+});
+
