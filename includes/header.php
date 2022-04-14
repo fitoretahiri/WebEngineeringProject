ecce8c5d825af8efa45ed02eb8f2363ea0b5c637
Fitore Tahiri <ttahirifitore@gmail.com>
2022-04-14 14:31:25 +0200

    reverting deleted files

--- includes/header.php
+++ includes/header.php
@@ -7,9 +7,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< HEAD
     <link rel="stylesheet" href="../CSS/style.css">
     <link rel="stylesheet" href="../CSS/logIn.css">
+<<<<<<< HEAD
     <link rel="stylesheet" href="../CSS/dashboard.css" >
+=======
+=======
+    <link rel="stylesheet" href="./CSS/style.css">
+    <link rel="stylesheet" href="./CSS/logIn.css">
+>>>>>>> a0d5dc007db53243496de25f8d911b9e556bcdd0
+>>>>>>> parent of cea3811 (strukturimi i files)
     <script src="https://kit.fontawesome.com/96651c389e.js" crossorigin="anonymous"></script>
     <title>Shop</title>
 </head>
@@ -20,7 +28,15 @@
         <nav class="nav-bar">
             <div class="logo-div"><h1 id="logo"><em>Furniture</em></h1></div>
             <div class="nav-list">
+<<<<<<< Updated upstream
+<<<<<<< HEAD
 
+=======
+            <!--h1 id="logo"><em>Furniture</em></h1-->
+>>>>>>> a0d5dc007db53243496de25f8d911b9e556bcdd0
+=======
+
+>>>>>>> Stashed changes
             <?php
                 $style = "";
                 if(isset($_SESSION['roli'])){
@@ -32,11 +48,29 @@
                     <li><a href="order.php">Order</a></li>
                     <li> <a href="news.php">News</a></li> 
                     <li><a href="contactPage.php">Contact</a></li>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+                    <li> <a <?php echo $style;?> href="../pages/logIn.php">Log in</a></li>
+                    <li><a <?php echo $style;?> href="../pages/register.php">Register</a></li>
+=======
+                    <li> <a <?php echo $style;?> href="logIn.php">Log in</a></li>
+                    <li><a <?php echo $style;?> href="register.php">Register</a></li>
+>>>>>>> a0d5dc007db53243496de25f8d911b9e556bcdd0
+>>>>>>> parent of cea3811 (strukturimi i files)
                     <?php
                         if (isset($_SESSION["roli"]) && $_SESSION["roli"] == 'Admin') {
                     ?>
                      <li>
+<<<<<<< HEAD
                         <a href="dashboard.php">
+=======
+<<<<<<< HEAD
+                        <a href="../views/dashboard.php">
+=======
+                        <a href="./views/dashboard.php">
+>>>>>>> a0d5dc007db53243496de25f8d911b9e556bcdd0
+>>>>>>> parent of cea3811 (strukturimi i files)
                             Dashboard
                         </a>
                     </li>
@@ -50,8 +84,16 @@
                         if (isset($_SESSION["roli"])) {
                     ?>
                      <li>
+<<<<<<< HEAD
                         <a href="../pages/logout.php">
+<<<<<<< HEAD
                             LogOut
+=======
+=======
+                        <a href="logout.php">
+>>>>>>> a0d5dc007db53243496de25f8d911b9e556bcdd0
+                            Log Out
+>>>>>>> parent of cea3811 (strukturimi i files)
                         </a>
                     </li>
                     <?php
---
+++ index.php
@@ -0,0 +1,130 @@
+<?php
+    include('includes/header.php');
+?>
+
+
+
+
+       <div class="carousel">
+            <div class="inside-carousel" id="first">    
+                <div class="slider-text">
+                    <h2 class="white-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, ipsam.</h2>
+                    <p class="red-text">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                    <button class="butoni"> <a href="#" class="link-style">Order now!</a></button>    
+                </div>
+            </div>
+
+            <div class="inside-carousel" id="second">    
+                <div class="slider-text">
+                    <h2 class="white-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, ipsam.</h2>
+                    <p class="red-text">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                    <button class="butoni"> <a href="#" class="link-style">Order now!</a></button>  
+                </div>
+                
+            </div>
+
+            <div class="inside-carousel" id="third">    
+                <div class="slider-text"> 
+                    <h2 class="white-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, ipsam.</h2>
+                    <p class="red-text">Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+                    <button class="butoni"> <a href="#" class="link-style">Order now!</a></button>    
+                </div>
+            </div>
+         </div>
+
+
+        <div class="most-liked">
+          <!--  <h1 class="paragrafi">Disa nga produktet me te pelqyera!</h1-->
+            <div class="div-most-liked">
+                <div class="produktet">
+                    <img src="HomePics/kitchen.jpg" alt="" class="foto">
+                    <p></p>
+                    <button class="order">order</button>
+                </div>
+                <div class="produktet">
+                    <img src="HomePics/chair.jpg" alt="" class="foto">
+                    <p></p>
+                    <button class="order">order</button>
+                </div>
+            </div>
+            <div class="div-most-liked">  
+                <div class="produktet">
+                    <img src="HomePics/lamp.jpeg" alt="" class="foto">
+                    <p></p>
+                    <button class="order">order</button>
+                </div>
+                <div class="produktet">
+                    <img src="HomePics/sofa.jpg" alt="" class="foto">
+                    <p></p>
+                    <button class="order">order</button>
+                </div>
+            </div>
+        </div>
+        <h1 class="paragrafi">What others say about us!</h1>
+        <div class="others-say">
+           
+            <div class="div-holder" id="iPari">
+                <div class="div-circle">
+                    <img src="HomePics/tim.jpg" alt="" class="Fotot-rreth">   
+                </div>  
+                
+                <div class="paragrafi-div" id="paragrafi-iPari">
+                    <h1 class="emri">Timothee Chalamet</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, aperiam.</p>
+                </div>    
+            </div>
+            <!--div i dyte-->
+            <div class="div-holder" id="iDyti">    
+                <div class="div-circle">
+                    <img src="HomePics/dylan.jpg" alt="" class="Fotot-rreth">
+                </div>   
+                <div class="paragrafi-div" id="paragrafi-iDyti">
+                    <h1 class="emri">Dylan O'Brien</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, magni?</p>
+                </div>             
+            </div>
+            <!--div i trete-->
+            <div class="div-holder" id="iTreti">
+                <div class="div-circle">
+                    <img src="HomePics/johnny.jpg" alt="" class="Fotot-rreth">
+                </div>  
+                <div class="paragrafi-div" id="paragrafi-iTreti">
+                    <h1 class="emri">Johnny Depp</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, magni?</p>
+                </div>                  
+            </div>
+        </div>
+
+        <div class="sales">
+            <h1 class="paragrafi">Produktet ne zbritje!</h1>
+            <div class="div-most-liked">
+                <div class="last-div">
+                    <img src="HomePics/hLamp.jpg" alt="" class="foto">
+                    <br>
+                    <button class="order">order</button>
+                </div>
+                <div class="last-div">
+                    <img src="HomePics/sofa.jpg" alt="" class="foto">
+                    <br>
+                    <button class="order">order</button>
+                </div>
+            </div>
+            <div class="div-most-liked">  
+                <div class="last-div">
+                    <img src="HomePics/lamp.jpeg" alt="" class="foto">
+                    <br>
+                    <button class="order">order</button>
+                </div>
+                <div class="last-div">
+                    <img src="HomePics/chair.jpg" alt="" class="foto">
+                    <br>
+                    <button class="order">order</button>
+                </div>
+            </div>
+            <a href="order.html" id="kliko">Buy now>></a>
+        </div>
+
+        <script src="JS/home.js"></script>
+
+<?php include 'includes/footer.php';
+?>       
